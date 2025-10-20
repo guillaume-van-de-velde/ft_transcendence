f85046033632cd5e0be13781c1e49879ac3dd39f
Guillaume VAN DE VELDE <guillaumevandevelde.perso@gmail.com>
2025-10-20 10:37:32 +0200

    delete build

--- ft_transcendence/front/build/components/utils/htmlTools.js
+++
@@ -1,6 +0,0 @@
-export function stringToHTMLElement(html) {
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    const element = template.content.firstElementChild;
-    return element;
-}
